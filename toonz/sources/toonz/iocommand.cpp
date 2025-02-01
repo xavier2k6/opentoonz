d03838ebe767eb149a28ada6f9091c0f2841ef30
Abel You <abelyou032@gmail.com>
2025-02-01 09:29:05 +0800

    Add a comment
    // Don't store current cleanup parameters to scene's parameters' cache if autosave
    // (would save to scene file) .

--- toonz/sources/toonz/iocommand.cpp
+++ toonz/sources/toonz/iocommand.cpp
@@ -1514,6 +1514,8 @@ bool IoCmd::saveScene(const TFilePath &path, int flags) {
 #endif
   }
 
+  // Don't store current cleanup parameters to scene's parameters' cache if autosave
+  // (would save to scene file) .
   if (!isAutosave) {
     CleanupParameters::GlobalParameters.assign(
         scene->getProperties()->getCleanupParameters());
